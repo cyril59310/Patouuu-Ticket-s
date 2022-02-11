7317b87e4b80912f79cafff9e87642c81a52cf9f
cyril59310 <archas.cyril@hotmail.fr>
2022-02-12 00:40:10 +0100

    FIX add/remove commands Overwrites permission

--- commands/add.js
+++ commands/add.js
@@ -13,23 +13,10 @@ module.exports = {
   async execute(interaction, client) {
     const chan = client.channels.cache.get(interaction.channelId);
     const user = interaction.options.getUser('target');
-    if (!interaction.member.roles.cache.find(r => r.id === client.config.roleSupport)) return interaction.reply({ content: "Vous devez avoir le role <@&" + client.config.roleSupport + "> .", ephemeral: true })
     if (chan.name.includes('ticket')) {
-      chan.edit({
-        permissionOverwrites: [{
-          id: user,
-          allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-        },
-        {
-          id: interaction.guild.roles.everyone,
-          deny: ['VIEW_CHANNEL'],
-        },
-        {
-          id: client.config.roleSupport,
-          allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-        },
-        ],
-      }).then(async () => {
+        chan.permissionOverwrites.create(
+          user,{SEND_MESSAGES:true,VIEW_CHANNEL:true}
+        ).then(async () => {
         interaction.reply({
           content: `<@${user.id}> a été ajouté au ticket !`
         });
--- commands/remove.js
+++ commands/remove.js
@@ -13,23 +13,10 @@ const {
     async execute(interaction, client) {
       const chan = client.channels.cache.get(interaction.channelId);
       const user = interaction.options.getUser('target');
-      if (!interaction.member.roles.cache.find(r => r.id === client.config.roleSupport)) return interaction.reply({content: "Vous devez avoir le role <@&" + client.config.roleSupport + "> .", ephemeral: true})
       if (chan.name.includes('ticket')) {
-        chan.edit({
-          permissionOverwrites: [{
-            id: user,
-            deny: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-          },
-          {
-            id: interaction.guild.roles.everyone,
-            deny: ['VIEW_CHANNEL'],
-          },
-          {
-            id: client.config.roleSupport,
-            allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-          },
-        ],
-        }).then(async () => {
+        chan.permissionOverwrites.create(
+          user,{SEND_MESSAGES:false,VIEW_CHANNEL:false}
+        ).then(async () => {
           interaction.reply({
             content: `<@${user.id}> a été retiré du ticket !`
           });
